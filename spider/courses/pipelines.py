9f89b1363be15abf41c4102970b9aedf35508898
Tianwe CHEN <tianwen.chen@hotmail.com>
2018-02-06 08:52:30 +0000

    parse only when necessary

--- spider/courses/pipelines.py
+++ spider/courses/pipelines.py
@@ -7,15 +7,13 @@
 
 import pymongo
 import logging
+import re
 
-class CoursesPipeline(object):
-    def process_item(self, item, spider):
-        return item
-
-class MongoPipeline(object):
+class CoursePipeline(object):
 
     def __init__(self, mongo_uri):
         self.mongo_uri = mongo_uri
+        self.course_set = set()
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -28,18 +26,64 @@ class MongoPipeline(object):
             authSource='courses_db'
         )
         self.db = self.client['courses_db']
-        docs = self.db['courses'].aggregate([
-            {
-                '$group': {
-                    '_id': '$code',
-                    'semesters': { '$push': '$semester' }
-                }
+
+    def close_spider(self, spider):
+        self.client.close()
+
+    def fix_case(self, key):
+        # remove characters '(', ')' and '-'
+        s = key.translate({ord(c): '' for c in '()-'})
+        # using regular expression
+        #s = re.sub(r'[()-]', '', key)
+        res = s.title().replace(' ', '')
+        return res[0].lower() + res[1:]
+
+    def process_item(self, item, spider):
+        code = item['code']
+        semester = item['semester']
+        selector = item['selector']
+        if (code, semester) not in self.course_set:
+            course = {
+                'code': code,
+                'semester': semester
             }
-        ])
-        self.code_semesters = {
-            doc['_id']: doc['semesters']
-            for doc in docs
-        }
+            header = selector.xpath('.//h2/text()').extract_first()
+            i = header.find('-')
+            j = header.rfind('(')
+            k = header.rfind('unit')
+            course['title'] = header[i+1:j].strip()
+            # credits may not be integer
+            # example: 2017-18 Fall EMBA5590
+            course['credits'] = float(header[j+1:k])
+            for tr in selector.xpath('.//div[contains(@class, "courseattr")]/div/table/tr'):
+                key = self.fix_case(' '.join(tr.xpath('.//th//text()').extract()))
+                value = '\t'.join([
+                    x.strip()
+                    for x in tr.xpath('.//td//text()').extract()
+                ])
+                course[key] = value
+            self.course_set.add((code, semester))
+            self.db.courses.insert_one(course)
+        item['selector'] = selector.xpath('.//table[@class="sections"]//tr')[1:]
+        return item
+
+class SectionPipeline(object):
+
+    def __init__(self, mongo_uri):
+        self.mongo_uri = mongo_uri
+        self.section_set = set()
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(mongo_uri=crawler.settings.get('MONGO_URI'))
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri,
+            username='comp4332',
+            password='bigdata',
+            authSource='courses_db'
+        )
+        self.db = self.client['courses_db']
 
     def close_spider(self, spider):
         self.client.close()
@@ -47,22 +91,84 @@ class MongoPipeline(object):
     def process_item(self, item, spider):
         code = item['code']
         semester = item['semester']
-        if code not in self.code_semesters:
-            self.db['course_codes'].insert_one({'code': code})
-            self.code_semesters[code] = []
-        if semester not in self.code_semesters[code]:
-            self.db['courses'].insert_one(item)
-            self.code_semesters[code].append(semester)
-        else:
-            self.db['courses'].update_one(
-                {'code': code, 'semester': semester},
-                {'$push': {
-                        'sections': {
-                            '$each': item['sections']
+        selector = item['selector']
+        record_time = item['record_time']
+        sections = []
+        prev_sect = None
+        for tr in selector:
+            class_name = tr.xpath('./@class').extract_first()
+            if 'newsect' in class_name:
+                sectionId = tr.xpath('./td[1]/text()').extract_first().split('(', 1)[0].strip()
+                m = re.match(r'\w+\d+', sectionId)
+                if m == None:
+                    print(code, sectionId)
+                section = {
+                    'sectionId': sectionId,
+                    'offerings': [
+                        {
+                            # most complicated case: 2016-17 Summer MGMT5410
+                            'dateAndTime': ' '.join(tr.xpath('./td[2]/text()').extract()),
+                            'room': tr.xpath('./td[3]/text()').extract_first(),
+                            'instructors': tr.xpath('./td[4]/text()').extract()
+                        }
+                    ],
+                    # the text may be inside its children
+                    # example: 2016-17 Spring ACCT5140 L1
+                    'quota': int(tr.xpath('./td[5]//text()').extract_first()),
+                    'enrol': int(tr.xpath('./td[6]//text()').extract_first()),
+                    # if avail is 0, it is enclosed by <strong>
+                    #'avail': int(tr.xpath('./td[7]//text()').extract_first()),
+                    'wait': int(tr.xpath('./td[8]//text()').extract_first())
+                }
+                remarks = '\t'.join([
+                    text.strip()
+                    for text in tr.xpath('./td[9]//text()').extract()
+                    if text.strip() != ''
+                ])
+                if remarks != '':
+                    section['remarks'] = remarks
+                sections.append(section)
+                prev_sect = section
+            else:
+                offering = {
+                    # index starts from 1
+                    # TODO: split dateAndTime to daysOfWeek and time
+                    'dateAndTime': ' '.join(tr.xpath('./td[1]/text()').extract()),
+                    'room': tr.xpath('./td[2]/text()').extract_first(),
+                    'instructors': tr.xpath('./td[3]/text()').extract()
+                }
+                prev_sect['offerings'].append(offering)
+        for section in sections:
+            sectionId = section['sectionId']
+            if (code, semester, sectionId) not in self.section_set:
+                self.section_set.add((code, semester, sectionId))
+                section_data = {
+                    'sectionId': sectionId,
+                    'offerings': section['offerings']
+                }
+                if 'remarks' in section:
+                    section_data['remarks'] = section['remarks']
+                self.db.courses.update_one(
+                    { 'code': code, 'semester': semester },
+                    { '$push': {
+                            'sections': section_data
                         }
                     }
+                )
+            self.db.courses.update_one(
+                { 'code': code, 'semester': semester, 'sections.sectionId': sectionId },
+                { '$push': {
+                        'sections.$.snapshots': {
+                            'recordTime': record_time,
+                            'quota': section['quota'],
+                            'enrol': section['enrol'],
+                            'wait': section['wait']
+                        }
+                    }
+
                 }
             )
+        return item
 
 class CompletePipeline(object):
     def close_spider(self, spider):
--- spider/courses/spiders/mongo_spider.py
+++ spider/courses/spiders/mongo_spider.py
@@ -8,11 +8,12 @@ import logging
 
 class MongoSpider(scrapy.Spider):
     name = 'mongo'
-    start_urls = ['http://comp4332.gitlab.io']
+    start_urls = ['http://localhost/snapshots.html']
 
     custom_settings = {
         'ITEM_PIPELINES': {
-            'courses.pipelines.MongoPipeline': 800,
+            'courses.pipelines.CoursePipeline': 700,
+            'courses.pipelines.SectionPipeline': 800,
             'courses.pipelines.CompletePipeline': 900
         }
     }
@@ -40,83 +41,14 @@ class MongoSpider(scrapy.Spider):
         # m = re.search(r'(.*) \w+: Snapshot taken at (.*)', title)
         # semester = m.group(1)
         # record_time = datetime.strptime(m.group(2), '%Y-%m-%d %H:%M')
-        #record_time = pytz.timezone('Hongkong').localize(record_time, is_dst=None)
+        record_time = pytz.timezone('Hongkong').localize(record_time, is_dst=None)
         courses = response.xpath('//div[@class="course"]')
         for course in courses:
-            yield self.parse_course(course, semester, record_time)
-
-    def parse_course(self, el, semester, record_time):
-        course = { 'semester': semester }
-        header = el.xpath('.//h2/text()').extract_first()
-        i = header.find('-')
-        j = header.rfind('(')
-        k = header.rfind('unit')
-        course['code'] = header[:i].replace(' ', '')
-        course['title'] = header[i+1:j].strip()
-        # credits may not be integer
-        # example: 2017-18 Fall EMBA5590
-        course['credits'] = float(header[j+1:k])
-        # simpler method using regular expression
-        # m = re.search(r'(.*?)\s-\s(.*?)\s\(([\d\.]+)\s*unit[s]?\)', header)
-        # course['code'] = m.group(1).replace(' ', '')
-        # course['title'] = m.group(2).strip()
-        # course['credits'] = float(m.group(3))
-        logging.debug('Parsing course: ' + course['code'])
-        for tr in el.xpath('.//div[contains(@class, "courseattr")]/div/table/tr'):
-            key = self.fix_case(' '.join(tr.xpath('.//th//text()').extract()))
-            value = '\t'.join([
-                x.strip()
-                for x in tr.xpath('.//td//text()').extract()
-            ])
-            course[key] = value
-        sections = []
-        prev_sect = None
-        for tr in el.xpath('.//table[@class="sections"]//tr')[1:]:
-            class_name = tr.xpath('./@class').extract_first()
-            if 'newsect' in class_name:
-                section = {
-                    'section': tr.xpath('./td[1]/text()').extract_first(),
-                    'offerings': [
-                        {
-                            # most complicated case: 2016-17 Summer MGMT5410
-                            'dateAndTime': ' '.join(tr.xpath('./td[2]/text()').extract()),
-                            'room': tr.xpath('./td[3]/text()').extract_first(),
-                            'instructors': tr.xpath('./td[4]/text()').extract()
-                        }
-                    ],
-                    # the text may be inside its children
-                    # example: 2016-17 Spring ACCT5140 L1
-                    'quota': int(tr.xpath('./td[5]//text()').extract_first()),
-                    'enrol': int(tr.xpath('./td[6]//text()').extract_first()),
-                    # if avail is 0, it is enclosed by <strong>
-                    #'avail': int(tr.xpath('./td[7]//text()').extract_first()),
-                    'wait': int(tr.xpath('./td[8]//text()').extract_first())
-                }
-                remarks = '\t'.join([
-                    text.strip()
-                    for text in tr.xpath('./td[9]//text()').extract()
-                    if text.strip() != ''
-                ])
-                if remarks != '':
-                    section['remarks'] = remarks
-                sections.append(section)
-                prev_sect = section
-            else:
-                offering = {
-                    # index starts from 1
-                    # TODO: split dateAndTime to daysOfWeek and time
-                    'dateAndTime': ' '.join(tr.xpath('./td[1]/text()').extract()),
-                    'room': tr.xpath('./td[2]/text()').extract_first(),
-                    'instructors': tr.xpath('./td[3]/text()').extract()
-                }
-                prev_sect['offerings'].append(offering)
-        course['sections'] = sections
-        return course
-
-    def fix_case(self, key):
-        # remove characters '(', ')' and '-'
-        s = key.translate({ord(c): '' for c in '()-'})
-        # using regular expression
-        #s = re.sub(r'[()-]', '', key)
-        res = s.title().replace(' ', '')
-        return res[0].lower() + res[1:]
+            header = course.xpath('.//h2/text()').extract_first()
+            item = {
+                'code': header.split('-', maxsplit=1)[0].replace(' ', ''),
+                'semester': semester,
+                'record_time': record_time,
+                'selector': course
+            }
+            yield item
